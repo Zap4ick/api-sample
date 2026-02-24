b7b0643966346796f40c41bbeace69f518a20bfc
andrei.lazuk <andrei.lazuk@onde.app>
2026-02-24 08:15:18 +0100

    minor fix: storing config keys in enum now

--- src/test/java/utils/TestConfig.java
+++ src/test/java/utils/TestConfig.java
@@ -6,6 +6,25 @@ import java.util.Properties;
 
 public class TestConfig {
     private static final Properties PROPERTIES = new Properties();
+    private static final String BASE_URL_ENV_VAR_KEY = "BASE_URL";
+
+    private enum ConfigKey {
+        BASE_URL("base.url"),
+        SUPERVISOR_LOGIN("supervisor.login"),
+        SUPERVISOR_ID("supervisor.id"),
+        ADMIN_LOGIN("admin.login"),
+        ADMIN_ID("admin.id");
+
+        private final String key;
+
+        ConfigKey(String key) {
+            this.key = key;
+        }
+
+        public String getKey() {
+            return key;
+        }
+    }
 
     static {
         try (InputStream is = TestConfig.class.getClassLoader().getResourceAsStream("test.properties")) {
@@ -19,26 +38,26 @@ public class TestConfig {
     }
 
     public static String getBaseUrl() {
-        String baseUrl = System.getenv("BASE_URL");
+        String baseUrl = System.getenv(BASE_URL_ENV_VAR_KEY);
         if (baseUrl != null && !baseUrl.isEmpty()) {
             return baseUrl;
         }
-        return PROPERTIES.getProperty("base.url");
+        return PROPERTIES.getProperty(ConfigKey.BASE_URL.getKey());
     }
 
     public static String getSupervisorLogin() {
-        return PROPERTIES.getProperty("supervisor.login");
+        return PROPERTIES.getProperty(ConfigKey.SUPERVISOR_LOGIN.getKey());
     }
 
     public static Long getSupervisorId() {
-        return Long.parseLong(PROPERTIES.getProperty("supervisor.id"));
+        return Long.parseLong(PROPERTIES.getProperty(ConfigKey.SUPERVISOR_ID.getKey()));
     }
 
     public static String getAdminLogin() {
-        return PROPERTIES.getProperty("admin.login");
+        return PROPERTIES.getProperty(ConfigKey.ADMIN_LOGIN.getKey());
     }
 
     public static Long getAdminId() {
-        return Long.parseLong(PROPERTIES.getProperty("admin.id"));
+        return Long.parseLong(PROPERTIES.getProperty(ConfigKey.ADMIN_ID.getKey()));
     }
 }
